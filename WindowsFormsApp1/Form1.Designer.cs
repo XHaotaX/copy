a3f91baa19b25806ee26dd73f6ca3a47955b72f6
XHaotaX <31614897+XHaotaX@users.noreply.github.com>
2021-03-02 15:12:31 +0300

    Add files via upload
    теперь точно только зип остался вроде

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,241 @@
+﻿namespace WindowsFormsApp3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStripProgressBar1 = new System.Windows.Forms.ToolStripProgressBar();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Enabled = false;
+            this.button1.Location = new System.Drawing.Point(103, 141);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "copy";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(204, 79);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(111, 17);
+            this.checkBox1.TabIndex = 5;
+            this.checkBox1.Text = "перезаписывать";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(328, 79);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(80, 17);
+            this.checkBox2.TabIndex = 6;
+            this.checkBox2.Text = "checkBox2";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            this.checkBox2.CheckedChanged += new System.EventHandler(this.checkBox2_CheckedChanged);
+            // 
+            // button2
+            // 
+            this.button2.Enabled = false;
+            this.button2.Location = new System.Drawing.Point(204, 141);
+            this.button2.Name = "button2";
+            this.button2.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "abort";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1,
+            this.toolStripProgressBar1});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 381);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(664, 22);
+            this.statusStrip1.TabIndex = 4;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.AutoSize = false;
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(400, 17);
+            this.toolStripStatusLabel1.Text = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // toolStripProgressBar1
+            // 
+            this.toolStripProgressBar1.Name = "toolStripProgressBar1";
+            this.toolStripProgressBar1.Size = new System.Drawing.Size(100, 16);
+            this.toolStripProgressBar1.Step = 1;
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.White;
+            this.textBox1.Font = new System.Drawing.Font("Arial Black", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox1.Location = new System.Drawing.Point(103, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(451, 23);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.TextChanged += new System.EventHandler(this.CheckPath);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(52, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(52, 42);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "label2";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(507, 10);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(47, 23);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "...";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(507, 39);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(47, 23);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "...";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Arial Black", 8.25F, System.Drawing.FontStyle.Bold);
+            this.textBox2.Location = new System.Drawing.Point(103, 39);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(451, 23);
+            this.textBox2.TabIndex = 3;
+            this.textBox2.TextChanged += new System.EventHandler(this.CheckPath);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(55, 198);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "label3";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(55, 215);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(35, 13);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "label4";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(664, 403);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.ToolStripProgressBar toolStripProgressBar1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,287 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Threading;
+
+namespace WindowsFormsApp3
+{
+    public partial class Form1 : Form
+    {
+        public List<string> Path=new List<string>();
+        Thread CopyThread;
+
+        public Form1()
+        {
+            InitializeComponent();
+            Path.Add("nani");
+            Path.Add("nani");
+            textBox1.Text = "E:/temp/Archives";
+            textBox2.Text = "E:/temp/qq";
+        }
+
+        private void toolStripSplitButton1_ButtonClick(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog temp = new FolderBrowserDialog();
+            if (temp.ShowDialog() == DialogResult.OK)
+            {
+                //Get the path of specified file
+                textBox2.Text = temp.SelectedPath;
+                Check(textBox2);
+                //Read the contents of the file into a stream
+            }
+
+        }
+        private void button3_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog temp = new FolderBrowserDialog();
+            if (temp.ShowDialog() == DialogResult.OK)
+            {
+                //Get the path of specified file
+                textBox1.Text = temp.SelectedPath;
+                Check(textBox1);
+                //Read the contents of the file into a stream
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            button1.Enabled = false;
+            checkBox1.Enabled = false;
+            CopyThread = new Thread(new ThreadStart(Copy));
+            CopyThread.Priority = ThreadPriority.Highest;
+            CopyThread.Start();
+            button2.Enabled = true;
+            //            Copy();
+            /*
+             * как првавило кнопа разблокирова мы ее блокируем
+             начало работы
+             еще раз проверяем пути
+                        
+            */
+
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            CopyThread.Abort();
+            button2.Enabled = false;
+            CheckPath(sender,e);
+        }
+
+        private void helo()
+        {
+            MessageBox.Show("HElo", "i work", MessageBoxButtons.OK,
+       MessageBoxIcon.Information, MessageBoxDefaultButton.Button1, MessageBoxOptions.DefaultDesktopOnly);
+        }
+
+        private bool Check(TextBox temp) {
+            if (!System.IO.Directory.Exists(temp.Text))
+            {
+                toolStripStatusLabel1.Text = "no correct" + temp.Text;
+                temp.BackColor = Color.FromArgb(255, 192, 192);
+                temp.ForeColor = Color.FromArgb(200, 40, 40);
+                toolStripStatusLabel1.ForeColor = Color.FromArgb(200, 40, 40);
+                return false;
+            }
+            else
+            {
+                toolStripStatusLabel1.Text = "correct";
+                temp.BackColor = Color.FromArgb(255, 255, 255);
+                temp.ForeColor = Color.FromArgb(50, 180, 50);
+                toolStripStatusLabel1.ForeColor = Color.FromArgb(40, 200, 40);
+//                button1.Enabled = true;
+                return true;
+            }
+        }
+        private void CheckPath(object sender, EventArgs e)
+        {
+            Path[0] = textBox1.Text;
+            Path[1] = textBox2.Text;
+            Check(textBox1);
+            Check(textBox2);
+            if (Check(textBox1)&&Check(textBox2))
+                button1.Enabled = true;
+            else
+                button1.Enabled = false;
+            //сдезь мути м проверку
+            /*вызывается при измении тескта в текст боксах
+             * и должно выводится соответвествуюшая инфа о коректности
+             * так же отвечает за блокировку кнопок начала работы
+             * по умолчанию блокированы
+             */
+        }
+
+        private void recursiveCreateAllNewDirectory(string dir)
+        {
+            var dirs = from i in Directory.EnumerateDirectories(dir)
+                       where true
+                       select i;
+            foreach (string cur_dir in dirs)
+            {
+                Directory.CreateDirectory(Path[1] + cur_dir.Substring(Path[0].Length));
+//                recursiveCreateAllNewDirectory(cur_dir); наверное это неправиль тк оно создает папки внутри а тедолжны создаватся и прикопировании файла послесоздания папки с годом
+            }
+        }
+
+        private void Copy() {
+            /*
+            err = 0;
+            k = 0;
+            MMax = 0;
+            Cf = 0;
+            */
+
+            //беру список директорий 
+            var dirs = from i in Directory.EnumerateDirectories(Path[0])
+                       where true
+                       select i;
+            //            label3.Text = dirs.LongCount().ToString()+"-";
+
+            recursiveCreateAllNewDirectory(Path[0]);
+            //захожу в каждую директорию
+            foreach (string cur_dir in dirs)
+            {
+                IEnumerable<string> files = from i in Directory.EnumerateFiles(cur_dir, "*.pdf", SearchOption.AllDirectories)
+                            where (/*File.GetCreationTime(i).Year*/File.GetLastWriteTime(i).Year <= DateTime.Now.Year)
+                            select i;
+//                BeginInvoke((MethodInvoker)(() => label3.Text = "-" + files.LongCount().ToString() ));//количество файлов в папке
+//                BeginInvoke((MethodInvoker)(()=>toolStripProgressBar1.Maximum = Convert.ToInt32(files.LongCount())));
+                Invoke((MethodInvoker)(() => toolStripProgressBar1.Maximum = Convert.ToInt32(files.LongCount())));
+//                BeginInvoke((MethodInvoker)(() => toolStripProgressBar1.Value = 0));
+                Invoke((MethodInvoker)(() => toolStripProgressBar1.Value = 0));
+                foreach (string file in files)
+                {
+                    //не понятно зачем label4.инвоке елси можно просто инвоке
+                    label4.Invoke((MethodInvoker)(() => label4.Text = file));
+//                    BeginInvoke((MethodInvoker)(() => toolStripProgressBar1.PerformStep()));
+                    Invoke((MethodInvoker)(() => toolStripProgressBar1.PerformStep()));
+                    if (!System.IO.Directory.Exists(Path[1] + cur_dir.Substring(Path[0].Length) + "/" + /*File.GetCreationTime(file).Year*/File.GetLastWriteTime(file).Year))
+                    {
+                        Directory.CreateDirectory(Path[1] + cur_dir.Substring(Path[0].Length)+ "/" + /*File.GetCreationTime(file).Year*/File.GetLastWriteTime(file).Year);
+                    }
+                    string fileto = Path[1] + cur_dir.Substring(Path[0].Length) + "/" + /*File.GetCreationTime(file).Year*/File.GetLastWriteTime(file).Year + "/"+ file.Substring(cur_dir.Length);
+                    //я это не хотел
+                    FileInfo f = new FileInfo(file);
+                    if (!System.IO.Directory.Exists(fileto.Substring(0,fileto.Length-f.Name.Length)))
+                    {
+                        Directory.CreateDirectory(fileto.Substring(0, fileto.Length - f.Name.Length));
+                    }
+
+                    //                    string fileto = Path[1] + File.GetCreationTime(file).Year + file.Substring(Path[0].Length);//тута сделать что надото. шта?соглашусь, шта? кажизьсь тута прсто копирование, бес сортивки погодам ппц.
+                    try
+                    { 
+                        File.Copy(file, fileto, checkBox1.Checked);
+                    }
+                    catch (System.IO.IOException)
+                    {
+                        continue;
+                    }
+                }
+            }
+            label4.Invoke((MethodInvoker)(() => label4.Text = "END"));
+            Invoke((MethodInvoker)(()=> checkBox1.Enabled = true));
+            MessageBox.Show("Закончено копирование","Сообшение о завершении", MessageBoxButtons.OK,
+        MessageBoxIcon.Information,MessageBoxDefaultButton.Button1, MessageBoxOptions.DefaultDesktopOnly);
+
+            /* сделано /\/\/\/\/\
+            /*
+            try
+            {
+                out_label['text'] = ("обнаружено" + str(MMax))////
+                }
+            catch BaseException
+            { c = 0; }
+            for direc in os.listdir(dir_src):{
+                direc_src = os.path.join(dir_src, direc)
+            //        print(direc_src)
+                    for root, dirs, files in os.walk(direc_src):
+                //        print ("[-]"+dir_src+dir_dst+str(stop))
+                            for name in files:{
+// print ("[-]"+dir_src+dir_dst)
+                        f_src = os.path.join(root, name)
+                                temptime = datetime.datetime.fromtimestamp(os.path.getmtime(f_src))
+                        try:
+                    //            print ("[-]"+dir_src+dir_dst)
+                                    k = k + 1
+                                        out_label['text'] = ("обработано " + str(k - 1) + "/" + str(MMax) + "\n" + f_src)////
+                                except BaseException:
+                                    c = 0
+                                    if temptime.year != date.year:{
+// путь к файлу назначения
+                            f_dst = dir_dst + "/" + direc + "/" + str(temptime.year) + "/" + (f_src[len(direc_src):len(root)]) + "/" + name
+                  //                    f_dst=dir_dst+f_src[len(dir_src):len(root)]+"/"++"/"+name
+                                    //путь к папке назначения
+                                    p_dst = dir_dst + "/" + direc + "/" + str(temptime.year) + "/" + (f_src[len(direc_src):len(root)])
+                  //                    p_dst=dir_dst+f_src[len(dir_src):len(root)]+"/"+str(temptime.year)+"/"
+                                    if not os.path.exists(f_dst):{
+// print ("[-] ", f_src)
+                                temp = datetime.datetime.now()
+                                        log.write("" + str(temp.day) + "/" + str(temp.month) + "/" + str(temp.year) + " " + str(temp.hour) + ":" + str(temp.minute) + ":" + str(temp.second) + " copy " + f_src + " to " + f_dst + "\n")
+                                        //создание директории если отсутсвует
+                                        if not os.path.exists(p_dst):
+                                            os.makedirs(p_dst)
+                                        shutil.copy2(f_src, f_dst)
+                                        Cf = Cf + 1
+                    //                    os.system("xcopy "+f_src+" "+p_dst+" /z /y /v /i")
+                    //                    log.write(str(subprocess.check_output("xcopy \""+f_src+"\" \""+p_dst+"\" /z /y /v")))
+                                        }
+                                    else:
+                                        if (zap == 0 and not filecmp.cmp(f_src, f_dst) and not compare_h(f_src) == compare_h(f_dst)){
+                                i = 0
+                                                name = name[:name.find('(')] + name[name.find(')'):]
+                                                while (os.path.exists(f_dst)){
+                    i = i + 1;
+                                                f_dst = p_dst + "/" + name[:name.find('.')] + "(" + str(i) + ")" + name[name.find('.'):]}
+// print(f_dst)
+                                shutil.copy2(f_src, f_dst)
+                                                Cf = Cf + 1}
+                            while (! filecmp.cmp(f_src, f_dst) and not compare_h(f_src) == compare_h(f_dst) && err<5){
+// print ("измененый файл , перезапись ", f_src)
+                                temp = datetime.datetime.now()
+                                              log.write("" + str(temp.day) + "/" + str(temp.month) + "/" + str(temp.year) + " " + str(temp.hour) + ":" + str(temp.minute) + ":" + str(temp.second) + " compare erro, rewrite " + f_src + " to " + f_dst + "\n")
+                          //                    log.write(str(subprocess.check_output("xcopy \""+f_src+"\" \""+p_dst+"\" /z /y /v")))
+                    //                    os.system("xcopy "+f_src+" "+p_dst+" /z /y /v /i")
+                    //                    subprocess.run(['xcopy', os.path.abspath(f_src), os.path.abspath(p_dst),'/s', '/e', '/z', '/y', '/v', '/i'],"log.log")
+
+                                        shutil.copy2(f_src, f_dst)
+                                              err = err + 1
+                          //                    shutil.copy2(f_src,f_dst)}
+                                    if (err == 5){
+                                        temp = datetime.datetime.now()
+                                          log.write("" + str(temp.day) + "/" + str(temp.month) + "/" + str(temp.year) + " " + str(temp.hour) + ":" + str(temp.minute) + ":" + str(temp.second) + " rewrite File: " + f_src + " failed\n")
+                                      }else{
+                                    temp = datetime.datetime.now()
+                                          log.write("" + str(temp.day) + "/" + str(temp.month) + "/" + str(temp.year) + " " + str(temp.hour) + ":" + str(temp.minute) + ":" + str(temp.second) + " " + str(err) + " rewrite " + f_src + "\n")
+                                          os.remove(f_src)
+                                          log.write(" remove " + f_src + "\n")}
+                                err = 0
+                          }
+                        }
+
+                        }*/
+            Invoke((MethodInvoker)(() => button2.Enabled = false));
+        }
+
+        private void checkBox2_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+    }
+}
